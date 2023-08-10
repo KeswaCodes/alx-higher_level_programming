f733e2447b1d0e229feb321d96f1e3fee79774fc
Zinhle <zinksw@gmail.com>
2023-08-10 16:32:35 -0700

    Fixes to pycodestyle format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,23 +9,21 @@ equal_to_0 = "and is 0"
 if (number < 0):
     number = number * -1
     last_digit = number % 10
-    negative_number = "-" + str(last_digit) 
+    negative_number = "-" + str(last_digit)
     if int(negative_number) < 6 and int(negative_number) != 0:
-            print(f"Last digit of -{number} is -{last_digit} {less_than_6_not_0}")
+        print(f"Last digit of -{number} is -{last_digit} {less_than_6_not_0}")
     elif negative_number > 5:
-            print(f"Last digit of -{number} is -{last_digit} {greater_than_5}")
+        print(f"Last digit of -{number} is -{last_digit} {greater_than_5}")
 
     else:
-            print(f"Last digit of -{number} is -{last_digit} {equal_to_0}")
-        
+        print(f"Last digit of -{number} is -{last_digit} {equal_to_0}")
 
 else:
     last_digit = number % 10
     if last_digit < 6 and last_digit != 0:
-            print(f"Last digit of {number} is {last_digit} {less_than_6_not_0}")
+        print(f"Last digit of {number} is {last_digit} {less_than_6_not_0}")
     elif last_digit > 5:
-            print(f"Last digit of {number} is {last_digit} {greater_than_5}")
-    
+        print(f"Last digit of {number} is {last_digit} {greater_than_5}")
+
     else:
-            print(f"Last digit of {number} is {last_digit} {equal_to_0}")
-    
+        print(f"Last digit of {number} is {last_digit} {equal_to_0}")
