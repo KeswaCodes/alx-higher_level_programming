f3d9f041024c267bb9193b3525dd26346ab9c81c
Zinhle <zinksw@gmail.com>
2023-08-11 03:35:58 -0700

    Fixes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ from add_0 import add as module
 if __name__ == "__main__":
     a = 1
     b = 2
-    result = module.add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, module.add(a, b)))
