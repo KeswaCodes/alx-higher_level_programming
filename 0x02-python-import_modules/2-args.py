74c836f5ba1c4110e9b699b11b648e0bacb9e9c1
Zinhle <zinksw@gmail.com>
2023-08-12 03:48:44 -0700

    Fixes to command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     i = 1
     args = len(sys.argv)
     args -= 1
-    if (len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         print("{} arguments".format(args))
 
+    elif len(sys.argv) == 2:
+        print("{} argument: ".format(args))
+        print("{}: {}".format(args, sys.argv[args]))
     else:
         print("{} arguments:".format(args))
         while i <= args:
