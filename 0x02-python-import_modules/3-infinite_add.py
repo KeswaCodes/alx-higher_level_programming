8d4ad16faaa1afde498c06954998f0729d15098f
Zinhle <zinksw@gmail.com>
2023-08-12 05:50:29 -0700

    Infinite addition with command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add
+if __name__ == "__main__":
+    result = 0
+    i = 1
+    if len(argv) == 1:
+        print(result)
+    else:
+        a = result
+        while i < len(argv):
+            result += int(argv[i])
+            i += 1
+    print(add(0, result))
+    
