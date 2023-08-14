7fcac18cb60d2d8970c98f2033bc84a60ee422f3
Zinhle <zinksw@gmail.com>
2023-08-14 00:21:10 -0700

    Replaces an element at specified position fixes

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,9 @@ def replace_in_list(my_list, idx, element):
     if idx is None or idx >= len(my_list):
         return my_list
     else:
-        del my_list[idx]
-        my_list.insert(idx, element)
-        return my_list
+        if element is None:
+            return my_list
+        else:
+            del my_list[idx]
+            my_list.insert(idx, element)
+            return my_list
