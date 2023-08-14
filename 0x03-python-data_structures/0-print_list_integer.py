cd0d0b19db6b5e44c6fffd327bb054fc7141da21
Zinhle <zinksw@gmail.com>
2023-08-13 23:37:18 -0700

    Fixes to print list int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if not my_list:
+    if my_list is None or len(my_list) == 0:
         return None
     else:
         for element in range(1, len(my_list) + 1):
