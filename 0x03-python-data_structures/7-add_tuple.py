72aafa3df2d750734d6a6a62ba35afb006b908fc
Zinhle <zinksw@gmail.com>
2023-08-14 13:49:58 -0700

    Adding two tuples - fixes to line count

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,21 +8,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif len(tuple_b) == 0:
             tuple_a = (0, 0)
 
-        else:
-            pass
-
     elif len(tuple_b) < 2:
         if len(tuple_b) == 1:
             tuple_b_first_int = tuple_b[0]
             tuple_b = (tuple_b_first_int, 0)
         elif len(tuple_b) == 0:
             tuple_b = (0, 0)
-
-        else:
-            pass
-
-    else:
-        pass
 
     first_element = tuple_a[0] + tuple_b[0]
     second_element = tuple_a[1] + tuple_b[1]
