960a0355e46f19a3f137baaa546c1679aebc0ec3
Zinhle <zinksw@gmail.com>
2023-08-14 13:44:17 -0700

    Adding two tuples fixes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_b) < 2:
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tuple_a_first_int = tuple_a[0]
+            tuple_a = (tuple_a_first_int, 0)
+        elif len(tuple_b) == 0:
+            tuple_a = (0, 0)
+
+        else:
+            pass
+        
+    elif len(tuple_b) < 2:
         if len(tuple_b) == 1:
             tuple_b_first_int = tuple_b[0]
             tuple_b = (tuple_b_first_int, 0)
