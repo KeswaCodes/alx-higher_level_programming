e996a65d22567405b038f47f652030cb7c83e0a7
Zinhle <zinksw@gmail.com>
2023-08-15 07:41:47 -0700

    Max integer fixes - empty list edge case fixed & pycodestyle format

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    
     max_num = my_list[0]
-    if len(my_list) == 0:
-        return None
+
     for number in my_list:
         if number > max_num:
             max_num = number
