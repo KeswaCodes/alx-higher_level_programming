4d9aca94c782879e305e1c0aec85248b23232422
Zinhle <zinksw@gmail.com>
2023-08-15 07:11:54 -0700

    Printing last digit of a number fixes - edge cases fixed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -6,6 +6,11 @@ def print_last_digit(number):
     if number < 0:
         number = number * -1
         last_digit += str(number % 10)
-    else:
+    elif number == 0:
+        last_digit += str(number)
+
+    elif number > 0:
         last_digit += str(number % 10)
+
     print("{}".format(last_digit), end='')
+    return last_digit
