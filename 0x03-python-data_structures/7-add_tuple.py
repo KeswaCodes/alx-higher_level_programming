603555fc0d95384d63222fd73e193596bf2760ae
Zinhle <zinksw@gmail.com>
2023-08-15 07:36:02 -0700

    Add tuple fixes - edge case fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,7 +8,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif len(tuple_a) == 0:
             tuple_a = (0, 0)
 
-    elif len(tuple_b) < 2:
+    if  len(tuple_b) < 2:
         if len(tuple_b) == 1:
             tuple_b_first_int = tuple_b[0]
             tuple_b = (tuple_b_first_int, 0)
