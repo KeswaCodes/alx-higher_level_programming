664cffac712a498b09e89a3af53cb8bba8e62290
Zinhle <zinksw@gmail.com>
2023-08-16 03:09:51 -0700

    Adding unique integers in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+
     if len(my_list) == 0:
         return None
+
     if search is None:
         return None
+
     final_list = my_list[:]
     for i in range(len(final_list)):
         if final_list[i] == search:
             final_list.remove(final_list[i])
+            if replace is None:
+                return None
+
             final_list.insert(i, replace)
 
     return final_list
