62dada291bc0e9a23f982b85ebcf601ac0bac016
Zinhle <zinksw@gmail.com>
2023-08-16 04:38:01 -0700

    Edge case fix - returning symmetric difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    if len(set_1) == 0 or len(set_2) == 0:
+    if set_1 is None:
+        return None
+
+    if set_2 is None:
         return None
 
     set_3 = set_1.symmetric_difference(set_2)
