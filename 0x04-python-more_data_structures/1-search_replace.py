e38cdee159055083e976a091d889c83bdf01e3b2
Zinhle <zinksw@gmail.com>
2023-08-16 02:58:22 -0700

    Replacing the occurances of an element by another in new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return None
+    if search is None:
+        return None
+    final_list = my_list[:]
+    for i in range(len(final_list)):
+        if final_list[i] == search:
+            final_list.remove(final_list[i])
+            final_list.insert(i, replace)
+
+    return final_list
