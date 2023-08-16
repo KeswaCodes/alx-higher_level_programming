dd74384c3660c937bcb475cf7207483d4a5938a6
Zinhle <zinksw@gmail.com>
2023-08-16 10:01:33 -0700

    Biggest value in dictionary - edge case fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,17 +5,18 @@ def best_score(a_dictionary):
 
     if len(a_dictionary) == 0:
         return None
-# get a list of tuples from dictionary
-    list_of_tuples = list(a_dictionary.items())
-    highest = list_of_tuples[0][1]
 
-# loop through tuple list to find highest value
-    for i in range(len(list_of_tuples)):
-        if type(list_of_tuples[i][1]) is not int:
-            return None
-        if list_of_tuples[i][1] > highest:
-            highest = list_of_tuples[i][1]
+    keys = list(a_dictionary.keys())
+    highest = a_dictionary.get(keys[0])
+    
+    for key in keys:
+       check = a_dictionary.get(key, None)
+       if check is None:
+           return None
 
-# index to the highest value
-    key = list_of_tuples[i - 1][0]
-    return key
+       if check > highest:
+           highest = check
+           key_highest = key
+
+
+    return key_highest
