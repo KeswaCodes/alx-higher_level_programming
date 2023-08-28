7c0fa0fa78a397829aa95b971dd1f7c2c30ef17f
Zinhle <zinksw@gmail.com>
2023-08-28 00:04:19 -0700

    Printing integers of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,10 +9,10 @@ def safe_print_list(my_list=[], x=0):
             i += 1
             list_len += 1
         print("{}".format(string))
-    
+
     except IndexError:
         print("{}".format(string))
-        
+
     finally:
 
         return list_len
