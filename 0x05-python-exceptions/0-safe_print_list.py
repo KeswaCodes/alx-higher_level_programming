0d41b15680a30e3976fef27e553e7074242a286a
Zinhle <zinksw@gmail.com>
2023-08-27 23:53:18 -0700

    Adding elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        list_len = 0
+        i = 0
+        string = ""
+        while i < x:
+            string += "".join(str(my_list[i]))
+            i += 1
+            list_len += 1
+        print("{}".format(string))
+    
+    except IndexError:
+        print("{}".format(string))
+        
+    finally:
+
+        return list_len
