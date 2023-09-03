88f5d5236768912c8c7b7f561bfd71f7e24ac0d4
Zinhle <zinksw@gmail.com>
2023-09-02 20:53:30 -0700

    Safekeeping, updates to be made soon

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 This module contains only one function
-
 FUNCTION
 add_integer(a, b) : adds two integers
-
 """
 def add_integer(a, b=98):
-    """this function adds two integers together"""
-        
+    """
+    this function adds two integers together
+    """
+
     if isinstance(a, (int, float)):
         a = int(a)
     else:
@@ -17,6 +17,5 @@ def add_integer(a, b=98):
         b = int(b)
     else:
         raise TypeError("b must be an integer")
-
         
     return a + b
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains the function for matrix_divided()
+
+matrix: list of lists
+div: number to divide by
+"""
+def matrix_divided(matrix, div):
+    
